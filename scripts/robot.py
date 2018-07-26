6e7a4bafea2ecb6795265a3c11aa41516cb8b6ff
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-07-26 20:44:54 +0900

    Add phantom

--- scripts/robot.py
+++ scripts/robot.py
@@ -198,24 +198,24 @@ class IdealCamera:                            ### fig:camera3
 
 # In[8]:
 
+if __name__ == '__main__':
+    world = World(30, 0.1) 
 
-world = World(30, 0.1) 
+    ### 地図を生成して3つランドマークを追加 ###
+    m = Map()                                  
+    m.append_landmark(Landmark(2,-2))
+    m.append_landmark(Landmark(-1,-3))
+    m.append_landmark(Landmark(3,3))
+    world.append(m)          
 
-### 地図を生成して3つランドマークを追加 ###
-m = Map()                                  
-m.append_landmark(Landmark(2,-2))
-m.append_landmark(Landmark(-1,-3))
-m.append_landmark(Landmark(3,3))
-world.append(m)          
+    ### ロボットを作る ###
+    straight = Agent(0.2, 0.0)    
+    circling = Agent(0.2, 10.0/180*math.pi)  
+    robot1 = IdealRobot( np.array([ 2, 3, math.pi/6]).T,    sensor=IdealCamera(m), agent=straight )             # 引数にcameraを追加、整理
+    robot2 = IdealRobot( np.array([-2, -1, math.pi/5*6]).T, sensor=IdealCamera(m), agent=circling, color="red")  # robot3は消しました
+    world.append(robot1)
+    world.append(robot2)
 
-### ロボットを作る ###
-straight = Agent(0.2, 0.0)    
-circling = Agent(0.2, 10.0/180*math.pi)  
-robot1 = IdealRobot( np.array([ 2, 3, math.pi/6]).T,    sensor=IdealCamera(m), agent=straight )             # 引数にcameraを追加、整理
-robot2 = IdealRobot( np.array([-2, -1, math.pi/5*6]).T, sensor=IdealCamera(m), agent=circling, color="red")  # robot3は消しました
-world.append(robot1)
-world.append(robot2)
-
-### アニメーション実行 ###
-world.draw()
+    ### アニメーション実行 ###
+    world.draw()
 
