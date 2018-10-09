24c08626c087488ef9ac674c29337cdfbf77ae84
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-10-09 17:02:47 +0900

    Small fix

--- scripts/puddle_world.py
+++ scripts/puddle_world.py
@@ -13,7 +13,7 @@ from mcl import *
 # In[2]:
 
 
-class Goal:  
+class Goal:  ###goal4
     def __init__(self, x, y, radius=0.3, value=0.0):
         self.pos = np.array([x, y]).T
         self.radius = radius
@@ -45,13 +45,13 @@ class Puddle:
         elems.append(ax.add_patch(r))
         
     def inside(self, pose):
-        return self.lowerleft[0] < pose[0] < self.upperright[0] and self.lowerleft[1] < pose[1] < self.upperright[1]
+        return all([ self.lowerleft[i] < pose[i] < self.upperright[i]  for i in [0, 1] ])
 
 
 # In[4]:
 
 
-class PuddleWorld(World):
+class PuddleWorld(World): ###puddleworld4
     def __init__(self, time_span, time_interval):
         super().__init__(time_span, time_interval)
         self.puddles = []
@@ -71,7 +71,7 @@ class PuddleWorld(World):
         super().one_step(i, elems, ax)
         for r in self.robots:
             r.puddle_depth = self.puddle_depth(r.pose)
-            for g in self.goals: #追加
+            for g in self.goals: #以下追加
                 if g.inside(r.pose):
                     r.in_goal = True
                     r.final_value = g.value
@@ -80,8 +80,8 @@ class PuddleWorld(World):
 # In[5]:
 
 
-class PuddleRobot(Robot):
-    def __init__(self, time_interval, pose, agent=None, sensor=None, color="black",                            noise_per_meter=5, noise_std=math.pi/60, bias_rate_stds=(0.1,0.1),                            expected_stuck_time=1e100, expected_escape_time = 1e-100,                           expected_kidnap_time=1e100, kidnap_range_x = (-5.0,5.0), kidnap_range_y = (-5.0,5.0),                            puddle_coef=10.0): 
+class PuddleRobot(Robot): ###puddlerobot4
+    def __init__(self, time_interval, pose, agent=None, sensor=None, color="black",                            noise_per_meter=5, noise_std=math.pi/60, bias_rate_stds=(0.1,0.1),                            expected_stuck_time=1e100, expected_escape_time = 1e-100,                           expected_kidnap_time=1e100, kidnap_range_x = (-5.0,5.0), kidnap_range_y = (-5.0,5.0),                            puddle_coef=100.0): 
         super().__init__(pose, agent, sensor, color, noise_per_meter, noise_std, bias_rate_stds,                          expected_stuck_time, expected_escape_time, expected_kidnap_time, kidnap_range_x, kidnap_range_y)
         
         self.puddle_depth = 0.0  
@@ -89,7 +89,7 @@ class PuddleRobot(Robot):
         self.total_reward = 0.0  
         self.time_interval = time_interval
         
-        self.in_goal = False
+        self.in_goal = False #以下2行追加
         self.final_value = 0.0
         
     def reward_per_sec(self):
@@ -112,9 +112,9 @@ class PuddleRobot(Robot):
 # In[6]:
 
 
-class PuddleIgnoreAgent(MclAgent):  ###mclagent5 (policy以下)
-    def __init__(self, time_interval, particle_pose, envmap, goal, particle_num=100,                 motion_noise_stds={"nn":0.19, "no":0.001, "on":0.13, "oo":0.2}): #goal追加
-        super().__init__(time_interval, 0.0, 0.0, particle_pose, envmap, particle_num, motion_noise_stds)
+class PuddleIgnoreAgent(MclAgent):  ###puddleignoreagent
+    def __init__(self, time_interval, particle_pose, envmap, goal, particle_num=100,                 motion_noise_stds={"nn":0.19, "no":0.001, "on":0.13, "oo":0.2}): #goal追加, 速度, 角速度の初期値は不要
+        super().__init__(time_interval, 0.0, 0.0, particle_pose, envmap, particle_num, motion_noise_stds) #速度, 角速度の初期値をゼロに
         
         self.goal = goal
         
@@ -122,20 +122,17 @@ class PuddleIgnoreAgent(MclAgent):  ###mclagent5 (policy以下)
     def policy(cls, pose, goal):
         x, y, theta = pose
         dx, dy = goal.pos[0] - x, goal.pos[1] - y
-        distance = math.sqrt(dx**2+ dy**2)                                   #ゴールまでの距離
         direction = int((math.atan2(dy, dx) - theta)*180/math.pi)   #ゴールの方角（degreeに直す）
-        direction = (direction + 360*1000 + 180)%360 - 180      #方角を-180〜180[deg]に正規化
+        direction = (direction + 360*1000 + 180)%360 - 180      #方角を-180〜180[deg]に正規化（適当。ロボットが-1000回転すると破綻）
         
         if direction > 10:      nu, omega = 0.0, 0.5
         elif direction < -10:  nu, omega = 0.0, -0.5
-        elif distance > 0.1:   nu, omega = 0.2, 0.0
-        else:                       nu, omega = 0.0, 0.0
+        else:                       nu, omega = 0.2, 0.0
             
         return nu, omega
         
     def decision(self, observation=None):
         nu, omega = self.policy(self.mcl.ml_pose, self.goal)
-        
         self.mcl.motion_update(nu, omega, self.time_interval)
         self.mcl.observation_update(observation)
         
@@ -165,9 +162,9 @@ if __name__ == '__main__': ###changetopuddlerobot4
     world.append(Puddle((-0.5, -2), (2.5, 1), 0.1))
 
     ### ロボットを作る ###
-    circling = PuddleIgnoreAgent(time_interval, np.array([0, 0, 0]).T, m, goal, particle_num=100)  #引数から速度、角速度を抜き、goalを追加
+    pia = PuddleIgnoreAgent(time_interval, np.array([0, 0, 0]).T, m, goal, particle_num=100)  #引数から速度、角速度を抜き、goalを追加
     r = PuddleRobot(time_interval, np.array([0,0,0]).T, sensor=Camera(m, distance_bias_rate_stddev=0, direction_bias_stddev=0),
-              agent=circling, color="red", bias_rate_stds=(0,0)) #ロボットを変更
+              agent=pia, color="red", bias_rate_stds=(0,0)) #ロボットを変更
 
     world.append(r)
     
